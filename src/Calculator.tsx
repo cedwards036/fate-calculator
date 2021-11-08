8cfc6d73f71a68b462f6dc54c1a1f651952c8cd4
Christopher Edwards <cedwards036@gmail.com>
2021-11-08 15:48:40 -0500

    add basic UI scaffold

---
+++ src/Calculator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Calculator() {
+    return (
+        <div className="calculator-wrapper">
+            <div className="calculator">
+                <h1>Fate Calculator</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Calculator;
